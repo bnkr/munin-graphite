09e365164a83082fa5ce1435665dccdf517916b3
James Webber <bunkerprivate@gmail.com>
2014-07-26 23:46:42 +0100

    Plugin processor with a standard and an overflow concurrency pool.

--- m2gpoll/poller.py
+++ m2gpoll/poller.py
@@ -2,6 +2,7 @@
 Parts of the polling algoritm.  Dealing with concurrency and so on.
 """
 from six.moves.queue import Queue
+from six.moves import queue
 import threading
 import logging
 
@@ -46,6 +47,11 @@ class EventQueue(object):
         that we crash horribly if the event producers stop working.  Ho hum."""
         return self.queue.get(block=self.block)
 
+class ThreadAdapter(logging.LoggerAdapter):
+    def process(self, msg, kwargs):
+        thread = threading.current_thread()
+        return u"Thread {0}: {1}".format(thread, msg), kwargs
+
 class ConcurrentHostPoller():
     """Polls a single host using a pool of runners.  This is intended to be run
     in its own thread and be sent events down a queue.  Usually each host would
@@ -56,7 +62,7 @@ class ConcurrentHostPoller():
         self.events = events
         self.pool = runners
         self.transports = transports
-        self.logger = logging.getLogger(__name__)
+        self.logger = ThreadAdapter(logging.getLogger(__name__), {})
 
     def run(self):
         """Wait for events and handle errors.  It's important that errors are
@@ -73,41 +79,176 @@ class ConcurrentHostPoller():
                 #   Need a config reload singal.
                 #
                 #   Deal with overlapping events (some plugins might be on a
-                #   different interval tho).
+                #   different interval tho).  Might need some kind of finish
+                #   event.
                 self.poll()
         except:
             self.logger.exception(u"poller exception")
             raise
         finally:
             try:
-                # It's sort of apropriate to exit here even though we didn't
-                # create the object because we shoudln't try and kill the pool
-                # until the host poller has finished using them.  Possibly the
-                # calling process might want to do more in order to avoid
-                # failure to terminate.
-                self.pool.exit_all()
+                # Possibly the calling process might want to do more to try and
+                # exit in order to avoid failure to terminate.
+                self.pool.finish()
             except:
                 self.logger.exception(
                     u"exception when closing runner pool forever now ({0} "
                     u"threads still running)".format(threading.active_count()))
-                # Bad things if we were already dealing with an exception I
-                # guess...
                 raise
 
     def poll(self):
         """Poll for plugins and dispatch jobs to process each one."""
-        self.pool.start_threads()
+        self.pool.start()
         transport = self.transports.get(thread_id=1)
 
         for plugin in transport.fetch():
-            # TODO:
-            #   Needs transport and config sent to plugin executor.
+            status = self.pool.run(EventQueue.Plugin(plugin))
+            if status == "full":
+                self.logger.warn("no space for plugin {0}".format(plugin))
+            else:
+                self.logger.debug("plugin {0} run: {1}".format(plugin, status))
 
-            # Sending an event rather than saying which function to run gives us
-            # a bit more flexibility in how to implement the pool.
-            self.logger.debug(u"push {0}".format(plugin))
-            self.pool.push_plugin(EventQueue.Plugin(plugin))
+class PluginRunner(object):
+    # exposition
+    def __init__(self, events):
+        self.events = events
 
-            # TODO:
-            #   needs some kind of logging at least if it hit the emergency pool
-            #   etc.
+    def run(self):
+        if self.events.get().is_exit():
+            return
+
+class LimitedPool(object):
+    """A thread pool of a particular size with a limited pipe to read from."""
+
+    def __init__(self, name, pool_size, queue_size, logger, thread):
+        self.pool_size = pool_size
+        self.queue_size = queue_size
+        self.events = Queue(maxsize=queue_size)
+        self.threads = []
+        self.name = name
+        self.logger = logger
+        self.thread_factory = thread
+
+    def create(self):
+        if self.threads:
+            return
+
+        self.logger.info(
+                u"starting {0} threads for {1}".format(self.pool_size, self.name))
+
+        runners = [PluginRunner(self.events) for _ in range(self.pool_size)]
+        self.threads = [
+                self.thread_factory(
+                    name="{0} #{1}/{2}".format(
+                        self.name, num + 1, self.pool_size),
+                    target=lambda: runner.run(),)
+                for num, runner in enumerate(runners)]
+
+    def start(self):
+        [thread.start() for thread in self.threads]
+
+    def put(self, event):
+        self.events.put(event, timeout=self.timeout)
+
+    def spam_exit(self):
+        self.logger.info("spam exit event")
+        for _ in self.threads:
+            try:
+                self.events.put(EventQueue.Exit(), block=False)
+            except queue.Full:
+                pass
+
+    def wait_for_exit(self):
+        # TODO:
+        #   Branches not well covered.
+
+        live_threads = [thread for thread in self.threads
+                        if thread.is_alive()]
+        while live_threads:
+            still_alive = []
+
+            for thread in live_threads:
+                try:
+                    self.events.put(EventQueue.Exit(), block=False)
+                except queue.Full:
+                    pass
+
+                self.logger.info("wait for {0}".format(thread))
+                thread.join(timeout=5)
+
+                # Quite often the threads won't exit, even if you wait a long
+                # time, so repeatedly spamming the event seems to be a good
+                # idea.
+                if thread.is_alive():
+                    still_alive.append(thread)
+                    self.logger.info("{0} did not exit".format(thread))
+                else:
+                    self.logger.info("{0} exitted".format(thread))
+
+            live_threads = still_alive
+
+class MultiPoolPluginProcessor():
+    """
+    Deals with a pool of plugin runners and the queues to send jobs to them.
+
+    This would normally be used in a pool per host.  Its main value is that we
+    can have an "overflow" pool for using when a plugin takes too long, meaning
+    you can keep parallelism down but still don't lose the entire set of metrics
+    for a host just because of one problem plugin.
+
+    Might be better with multiprocessing . thread . ThreadPool or
+    ThreadPoolExecutor (from py3 or library).  Certainly this will deal with
+    dead threads better.
+    """
+
+    def __init__(self, general, overflow, thread_factory=None):
+        self.logger = ThreadAdapter(logging.getLogger(__name__), {})
+
+        thread_factory = thread_factory or threading.Thread
+        self.general = LimitedPool(pool_size=general or 1, name="general",
+                                   logger=self.logger, thread=thread_factory,
+                                   queue_size=general or 1)
+        self.overflow = LimitedPool(pool_size=overflow or 0, name="overflow",
+                                    logger=self.logger, thread=thread_factory,
+                                    queue_size=overflow or 1)
+
+        self.overflow.timeout = 5
+        self.general.timeout = 2
+
+        self.finished = False
+        self.started = False
+
+    def start(self):
+        """Can be called multiple times."""
+        if self.started:
+            return
+
+        self.general.create()
+        self.overflow.create()
+
+        self.started = True
+
+    def finish(self):
+        """Finish with this pool.  Try our best to make sure all the threads
+        close."""
+
+        # Avoid filling up the queue if we already tried this one time.
+        if not self.finished:
+            self.general.spam_exit()
+            self.overflow.spam_exit()
+
+        self.finished = True
+        self.general.wait_for_exit()
+        self.overflow.wait_for_exit()
+
+    def run(self, plugin):
+        """Run the plugin using the general or overflow pools."""
+        try:
+            self.general.put(plugin)
+            return "queued"
+        except queue.Full:
+            try:
+                self.overflow.put(plugin)
+                return "overflopwed"
+            except queue.Full:
+                return "full"
--- tests/poller.py
+++ tests/poller.py
@@ -1,11 +1,13 @@
 from _util import TestCase, Mock
 
+import threading
+
 from m2gpoll.munin import Telnet as TelnetTransport
-from m2gpoll.poller import ConcurrentHostPoller, EventQueue
+from m2gpoll.poller import ConcurrentHostPoller, EventQueue, MultiPoolPluginProcessor
 
 class ConcurrentHostPollerTest(TestCase):
     def _make_poller(self):
-        pool = Mock()
+        pool = Mock(spec=MultiPoolPluginProcessor)
         transports = Mock()
         events = EventQueue(block=False)
 
@@ -21,22 +23,22 @@ class ConcurrentHostPollerTest(TestCase):
         poller = self._make_poller()
         poller.events.put(poller.events.Interval())
         poller.poll = Mock(side_effect=self.SomeError())
-        self.assertEquals(False, poller.pool.exit_all.called)
+        self.assertEquals(False, poller.pool.finish.called)
         self.assertRaises(self.SomeError, poller.run)
-        self.assertEquals(True, poller.pool.exit_all.called)
+        self.assertEquals(True, poller.pool.finish.called)
 
     def test_pool_crash_covered(self):
         poller = self._make_poller()
         poller.events.put(poller.events.Exit())
-        poller.pool.exit_all.side_effect = self.SomeError()
+        poller.pool.finish.side_effect = self.SomeError()
         self.assertRaises(self.SomeError, poller.run)
 
     def test_exit_on_event(self):
         poller = self._make_poller()
         poller.events.put(poller.events.Exit())
-        self.assertEquals(False, poller.pool.exit_all.called)
+        self.assertEquals(False, poller.pool.finish.called)
         poller.run()
-        self.assertEquals(True, poller.pool.exit_all.called)
+        self.assertEquals(True, poller.pool.finish.called)
 
     def test_poll_event(self):
         poller = self._make_poller()
@@ -46,6 +48,75 @@ class ConcurrentHostPollerTest(TestCase):
         transport.fetch.return_value = ['a', 'b', 'c',]
         poller.transports.get.return_value = transport
 
-        self.assertEquals(False, poller.pool.exit_all.called)
+        self.assertEquals(False, poller.pool.finish.called)
         poller.run()
-        self.assertEquals(True, poller.pool.exit_all.called)
+        self.assertEquals(True, poller.pool.finish.called)
+
+class MultiPoolPluginProcessorTest(TestCase):
+    def _make_pool(self, **kw):
+        args = dict(overflow=0, general=1)
+        args.update(kw)
+        pool = MultiPoolPluginProcessor(**args)
+        self.pools.append(pool)
+        return pool
+
+    def setUp(self):
+        self.pools = []
+
+    def tearDown(self):
+        for pool in self.pools:
+            try:
+                pool.finish()
+            except:
+                import traceback
+                traceback.print_exc()
+
+    def test_values_default_sensibly(self):
+        pool = self._make_pool(general=0, overflow=0)
+        self.assertEquals(1, pool.general.pool_size)
+        self.assertEquals(0, pool.overflow.pool_size)
+
+    def test_general_thread_pool_started(self):
+        pool = self._make_pool(general=2, overflow=0)
+        pool.start()
+        self.assertEquals(2, len(pool.general.threads))
+        self.assertEquals(0, len(pool.overflow.threads))
+        pool.finish()
+        self.assertEquals(False, any(thread.is_alive()
+                                     for thread in pool.general.threads))
+
+    def test_threads_are_not_joined_twice(self):
+        def make_thread(**kw):
+            mock = Mock(spec=threading.Thread)
+            mock.is_alive.return_value = True
+
+            def flip_alive(*args, **kw):
+                mock.is_alive.return_value = False
+
+            mock.join.side_effect = flip_alive
+            return mock
+
+        pool = self._make_pool(general=2, overflow=2,
+                               thread_factory=make_thread)
+        pool.overflow.timeout = 0
+        pool.general.timeout = 0
+        pool.start()
+        pool.finish()
+        self.assertEquals(set([1]), set([thread.join.call_count
+                                    for thread in pool.general.threads]))
+        pool.finish()
+        self.assertEquals(set([1]), set([thread.join.call_count
+                                    for thread in pool.general.threads]))
+
+    def test_plugin_executed(self):
+        pool = self._make_pool()
+        pool.run(EventQueue.Plugin("name"))
+        self.assertEquals("name", pool.general.events.get(block=False).name)
+
+    def test_plugin_overflowed(self):
+        pool = self._make_pool(general=2, overflow=1)
+        pool.general.timeout = 0 # avoid slow test
+        pool.run(EventQueue.Plugin("name"))
+        pool.run(EventQueue.Plugin("name"))
+        pool.run(EventQueue.Plugin("name"))
+        self.assertEquals("name", pool.overflow.events.get(block=False).name)
